fd0cfdc7d42b5cda13115919a4f90fb3f369d5cd
Douglas Gusson <douglasgusson@gmail.com>
2017-04-17 19:21:11 -0300

    correcoes na configuracao de banco de dados

--- src/br/com/pedidogm/view/FrmConfiguraBanco.java
+++ src/br/com/pedidogm/view/FrmConfiguraBanco.java
@@ -62,6 +62,7 @@ public class FrmConfiguraBanco extends javax.swing.JDialog {
         btAlterar = new javax.swing.JButton();
         btCancelar = new javax.swing.JButton();
         btImportar = new javax.swing.JButton();
+        jLabel6 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Configuração do Banco de Dados");
@@ -117,6 +118,10 @@ public class FrmConfiguraBanco extends javax.swing.JDialog {
             }
         });
 
+        jLabel6.setFont(new java.awt.Font("Dialog", 1, 10)); // NOI18N
+        jLabel6.setForeground(java.awt.Color.red);
+        jLabel6.setText("Obs.: Quaisquer alterações, exigem que o sistema seja reiniciado.");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -124,12 +129,6 @@ public class FrmConfiguraBanco extends javax.swing.JDialog {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel5, javax.swing.GroupLayout.Alignment.LEADING))
-                        .addGap(0, 0, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(tfNome)
@@ -155,7 +154,14 @@ public class FrmConfiguraBanco extends javax.swing.JDialog {
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(jLabel4)
                                     .addComponent(tfPorta, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                        .addContainerGap())))
+                        .addContainerGap())
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel5)
+                            .addComponent(jLabel6))
+                        .addGap(0, 0, Short.MAX_VALUE))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -181,6 +187,8 @@ public class FrmConfiguraBanco extends javax.swing.JDialog {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(tfSenha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel6)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btSair)
                     .addComponent(btGravar)
@@ -224,13 +232,11 @@ public class FrmConfiguraBanco extends javax.swing.JDialog {
             xstream.toXML(database, streamOut);
 
             JOptionPane.showMessageDialog(null,
-                    "Para que as alterações surtam efeito,\n"
-                    + "feche o sistema e abra novamente!",
-                    "Configurações registradas",
-                    JOptionPane.INFORMATION_MESSAGE);
+                    "Para que as alterações surtam efeito, o sistema será reiniciado!",
+                    "Configurações registradas", JOptionPane.INFORMATION_MESSAGE);
 
-            inicializarFrame();
-
+            System.exit(0);
+            
         } catch (FileNotFoundException ex) {
             Logger.getLogger(FrmConfiguraBanco.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
@@ -354,6 +360,7 @@ public class FrmConfiguraBanco extends javax.swing.JDialog {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JTextField tfHost;
     private javax.swing.JTextField tfNome;
     private javax.swing.JTextField tfPorta;
