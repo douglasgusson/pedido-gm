7987d3d319ba9b94e35fbcd1ddc1cd160c36d4be
Douglas Gusson <douglasgusson@gmail.com>
2017-07-06 19:31:18 -0300

    incremento de versao

--- src/br/com/pedidogm/util/Info.java
+++ src/br/com/pedidogm/util/Info.java
@@ -7,7 +7,7 @@ package br.com.pedidogm.util;
 public class Info {
 
     private static final String NAME_SYSTEM = "PedidoGM";
-    private static final String VERSION = "v0.9.12";
+    private static final String VERSION = "v0.9.13";
     private static StringBuffer buffer;
 
     public static String getVersion() {
